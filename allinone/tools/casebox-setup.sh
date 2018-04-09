874aa1c66d6cd151d7962aadcc7afcd92fa544de
Emerson Rocha Luiz <emerson@alligo.com.br>
2018-04-09 01:56:54 -0300

    allinone (#1): added config.ini file discovered via reverse engineering of the code; oficial documentation could be improved here

--- allinone/tools/casebox-setup.sh
+++ allinone/tools/casebox-setup.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-echo "/casebox-setup.sh: waiting..."
+echo "/tools/casebox-setup.sh: Starting. Wait some time for MySQL is running..."
 
-sleep 5
+# TODO: instead of a sleep, a good idea would be check when mysql is running (fititnt, 2018-04-09 01:23 BRT)
+
+sleep 5 # Remove later this one (needs 1s to not scare supervisord default time) (fititnt, 2018-04-09 01:19 BRT)
 
 RESULT=`mysqlshow --user=casebox --password="StrongPassword" casebox| grep -v Wildcard | grep -o casebox`
-if [ "$RESULT" == "myDatabase" ]; then
-    echo "/casebox-setup.sh: casebox database exists"
+if [ "$RESULT" == "casebox" ]; then
+    echo "/tools/casebox-setup.sh: casebox database exists"
 else
-    echo "/casebox-setup.sh: casebox database do not exist. Creating one..."
+    echo "/tools/casebox-setup.sh: casebox database do not exist. Creating one..."
+    bash -c /tools/init-database.sh
+fi
+
+RESULT=`mysqlshow --user=casebox --password="StrongPassword" casebox| grep -v Wildcard | grep -o casebox`
+if [ "$RESULT" != "casebox" ]; then
+    echo "/tools/casebox-setup.sh: CRITICAL, casebox still does not exist, and was not created"
+    echo "/tools/casebox-setup.sh: casebox-docker will not work"
+    exit 1
 fi
--- allinone/tools/init-database.sh
+++ allinone/tools/init-database.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-mysql -u root -p -e "CREATE DATABASE casebox; CREATE USER 'casebox'@'localhost' IDENTIFIED BY 'StrongPassword'; GRANT ALL PRIVILEGES ON casebox.* TO 'casebox'@'localhost'; FLUSH PRIVILEGES;"
+CASEBOX_DATABSE_SEED=install/mysql/_casebox.sql
+
+echo "/tools/init-database.sh: creating user and database"
+mysql -u root -e "CREATE DATABASE casebox; CREATE USER 'casebox'@'localhost' IDENTIFIED BY 'StrongPassword'; GRANT ALL PRIVILEGES ON casebox.* TO 'casebox'@'localhost'; FLUSH PRIVILEGES;"
+
+
+# NOTE: this command from (https://github.com/KETSE/casebox/wiki/Ubuntu-14.04-x64) does not work. I will use another SQL dump (fititnt, 2018-04-09 01:27 BRT)
+# mysql -u root -p casebox < /var/www/casebox/var/backup/cb_default.sql
+
+echo "/tools/init-database.sh: importing /var/www/casebox/$CASEBOX_DATABSE_SEED"
+
+mysql -u root casebox < "/var/www/casebox/$CASEBOX_DATABSE_SEED"
+
+
+echo "/tools/init-database.sh: done"
