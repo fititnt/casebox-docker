ad0ff36df5b39af31e03a750972edeefc857c668
Emerson Rocha Luiz <emerson@alligo.com.br>
2018-04-09 03:58:30 -0300

    allinone (#1): more testing. Some undocumented features found

--- allinone/tools/casebox-setup.sh
+++ allinone/tools/casebox-setup.sh
@@ -6,17 +6,17 @@ echo "/tools/casebox-setup.sh: Starting. Wait some time for MySQL is running..."
 
 sleep 5 # Remove later this one (needs 1s to not scare supervisord default time) (fititnt, 2018-04-09 01:19 BRT)
 
-RESULT=`mysqlshow --user=casebox --password="StrongPassword" casebox| grep -v Wildcard | grep -o casebox`
-if [ "$RESULT" == "casebox" ]; then
-    echo "/tools/casebox-setup.sh: casebox database exists"
+RESULT=`mysqlshow --user=casebox --password="StrongPassword" cb__casebox| grep -v Wildcard | grep -o cb__casebox`
+if [ "$RESULT" == "cb__casebox" ]; then
+    echo "/tools/casebox-setup.sh: cb__casebox database exists"
 else
-    echo "/tools/casebox-setup.sh: casebox database do not exist. Creating one..."
+    echo "/tools/casebox-setup.sh: cb__casebox database do not exist. Creating one..."
     bash -c /tools/init-database.sh
 fi
 
-RESULT=`mysqlshow --user=casebox --password="StrongPassword" casebox| grep -v Wildcard | grep -o casebox`
-if [ "$RESULT" != "casebox" ]; then
-    echo "/tools/casebox-setup.sh: CRITICAL, casebox still does not exist, and was not created"
+RESULT=`mysqlshow --user=casebox --password="StrongPassword" cb__casebox| grep -v Wildcard | grep -o cb__casebox`
+if [ "$RESULT" != "cb__casebox" ]; then
+    echo "/tools/casebox-setup.sh: CRITICAL, cb__casebox still does not exist, and was not created"
     echo "/tools/casebox-setup.sh: casebox-docker will not work"
     exit 1
 fi
--- allinone/tools/init-database.sh
+++ allinone/tools/init-database.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-CASEBOX_DATABSE_SEED=install/mysql/_casebox.sql
+CASEBOX_DATABSE_SEED1=install/mysql/bare_bone_core.sql
+CASEBOX_DATABSE_SEED2=install/mysql/_casebox.sql
 
 echo "/tools/init-database.sh: creating user and database"
-mysql -u root -e "CREATE DATABASE casebox; CREATE USER 'casebox'@'localhost' IDENTIFIED BY 'StrongPassword'; GRANT ALL PRIVILEGES ON casebox.* TO 'casebox'@'localhost'; FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE cb__casebox; CREATE USER 'casebox'@'localhost' IDENTIFIED BY 'StrongPassword'; GRANT ALL PRIVILEGES ON cb__casebox.* TO 'casebox'@'localhost'; FLUSH PRIVILEGES;"
 
 
 # NOTE: this command from (https://github.com/KETSE/casebox/wiki/Ubuntu-14.04-x64) does not work. I will use another SQL dump (fititnt, 2018-04-09 01:27 BRT)
 # mysql -u root -p casebox < /var/www/casebox/var/backup/cb_default.sql
 
-echo "/tools/init-database.sh: importing /var/www/casebox/$CASEBOX_DATABSE_SEED"
+echo "/tools/init-database.sh: importing /var/www/casebox/$CASEBOX_DATABSE_SEED1"
+mysql -u root cb__casebox < "/var/www/casebox/$CASEBOX_DATABSE_SEED1"
 
-mysql -u root casebox < "/var/www/casebox/$CASEBOX_DATABSE_SEED"
-
+echo "/tools/init-database.sh: importing /var/www/casebox/$CASEBOX_DATABSE_SEED2"
+mysql -u root cb__casebox < "/var/www/casebox/$CASEBOX_DATABSE_SEED2"
 
 echo "/tools/init-database.sh: done"
